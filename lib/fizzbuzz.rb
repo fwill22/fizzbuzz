1ed7632dae58377b85a6998d3b81037a2d944e13
fwill22 <79596926+fwill22@users.noreply.github.com>
2021-03-26 12:16:43 +0000

    Rspec Test Complete

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,16 +1,11 @@
 def fizzbuzz(number)
-  while number <= 99 do
-    number += 1
-    if number % 3 == 0 && number % 5 == 0
-    puts "fizzbuzz"
-    elsif number % 3 == 0
-      puts "fizz"
-    elsif number % 5 == 0
-      puts "buzz"
-    else 
-      puts number
-    end 
-  end
+  if number % 3 == 0 && number % 5 == 0
+    return "fizzbuzz"
+  elsif number % 3 == 0
+    return "fizz"
+  elsif number % 5 == 0
+    return "buzz"
+  else 
+    return number
+  end 
 end
-
-fizzbuzz(0)
